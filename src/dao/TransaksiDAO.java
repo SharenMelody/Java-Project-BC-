51e7aeb4475e2f3d75d80b82d17853ea2f88bdad
Jean <jeanarby030108@gmail.com>
2025-01-31 08:07:09 +0700

    TransaksiDAO - detailTransaksiDAO - detailTransaksi - string -> int

--- src/dao/TransaksiDAO.java
+++ src/dao/TransaksiDAO.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import koneksi.Koneksi;
 import model.Transaksi;
+import model.Detail_Transaksi;
 
 public class TransaksiDAO {
     Connection con;
@@ -14,6 +15,8 @@ public class TransaksiDAO {
     ResultSet rs;
     ArrayList<Transaksi> listTransaksi;
     Transaksi transaksi;
+    ArrayList<Detail_Transaksi> listDetailTransaksi;
+    Detail_Transaksi detailTransaksi;
 
     public TransaksiDAO() {
         con = Koneksi.getConnection();
@@ -106,5 +109,97 @@ public class TransaksiDAO {
             return transaksi = null;
         }
         return transaksi;
+    }
+    
+    public void setDetailTransaksi() throws SQLException {
+        detailTransaksi.setQty_produk(rs.getInt("qty_produk"));
+        detailTransaksi.setTransaksi_id(rs.getString("transaksi_id"));
+        detailTransaksi.setProduk_id(rs.getString("produk_id"));
+        detailTransaksi.setHarga_satuan(rs.getDouble("harga_satuan"));
+    }
+
+    // CRUD Methods
+
+    public ArrayList<Detail_Transaksi> getListDetailTransaksiByTransaksiId(String transaksi_id) {
+        try {
+            listDetailTransaksi = new ArrayList<>();
+            ps = con.prepareStatement("SELECT * FROM detail_transaksi WHERE transaksi_id = ?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ps.setString(1, transaksi_id);
+            rs = ps.executeQuery();
+            rs.beforeFirst();
+            while (rs.next()) {
+                detailTransaksi = new Detail_Transaksi();
+                setDetailTransaksi();
+                listDetailTransaksi.add(detailTransaksi);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e);
+        }
+        return listDetailTransaksi;
+    }
+
+    public void insertDetailTransaksi(String transaksi_id, String produk_id, int qty_produk, double harga_satuan) {
+        String query = "INSERT INTO detail_transaksi (transaksi_id, produk_id, qty_produk, harga_satuan) VALUES (?, ?, ?, ?)";
+        try {
+            ps = con.prepareStatement(query);
+            ps.setString(1, transaksi_id);
+            ps.setString(2, produk_id);
+            ps.setInt(3, qty_produk);
+            ps.setDouble(4, harga_satuan);
+            ps.executeUpdate();
+
+            System.out.println("Detail Transaksi berhasil ditambahkan");
+        } catch (SQLException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+
+    public void updateDetailTransaksi(String transaksi_id, String produk_id, int qty_produk, double harga_satuan) {
+        String query = "UPDATE detail_transaksi SET qty_produk = ?, harga_satuan = ? WHERE transaksi_id = ? AND produk_id = ?";
+        try {
+            ps = con.prepareStatement(query);
+            ps.setInt(1, qty_produk);
+            ps.setDouble(2, harga_satuan);
+            ps.setString(3, transaksi_id);
+            ps.setString(4, produk_id);
+            ps.executeUpdate();
+
+            System.out.println("Detail Transaksi berhasil diupdate");
+        } catch (SQLException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+
+    public void deleteDetailTransaksi(String transaksi_id, String produk_id) {
+        String query = "DELETE FROM detail_transaksi WHERE transaksi_id = ? AND produk_id = ?";
+        try {
+            ps = con.prepareStatement(query);
+            ps.setString(1, transaksi_id);
+            ps.setString(2, produk_id);
+            ps.executeUpdate();
+            System.out.println("Detail Transaksi berhasil dihapus");
+        } catch (SQLException e) {
+            System.out.println("Error: " + e);
+        }
+    }
+
+    public Detail_Transaksi getDetailTransaksiById(String transaksi_id, String produk_id) {
+        detailTransaksi = new Detail_Transaksi();
+        try {
+            ps = con.prepareStatement("SELECT * FROM detail_transaksi WHERE transaksi_id = ? AND produk_id = ?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ps.setString(1, transaksi_id);
+            ps.setString(2, produk_id);
+            rs = ps.executeQuery();
+            rs.beforeFirst();
+            if (rs.next()) {
+                setDetailTransaksi();
+            } else {
+                throw new SQLException("Detail Transaksi tidak ditemukan");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e);
+            return detailTransaksi = null;
+        }
+        return detailTransaksi;
     }
 }
